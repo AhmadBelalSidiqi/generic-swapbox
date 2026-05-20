bceb502a1d9f79265a8d431e29619ad4c5c80675
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-05-20 12:57:59 -0400

    Generic SwapBox class and generic static displayBox method created.

--- src/main/java/com/pluralsight/generics/Program.java
+++ src/main/java/com/pluralsight/generics/Program.java
@@ -1,4 +1,31 @@
 package com.pluralsight.generics;
 
 public class Program {
+    public static void main(String[] args) {
+        SwapBox<String> words = new SwapBox<>("Coffee", "Tea");
+        SwapBox<Integer> scores = new SwapBox<>(85, 92);
+        SwapBox<Double> prices = new SwapBox<>(4.99, 9.49);
+
+        System.out.println("-----------------Before Swapping---------");
+        System.out.println(words.describe());
+        System.out.println(scores.describe());
+        System.out.println(prices.describe());
+        System.out.println("-----------------After Swapping----------");
+        words.swap();
+        scores.swap();
+        prices.swap();
+        System.out.println(words.describe());
+        System.out.println(scores.describe());
+        System.out.println(prices.describe());
+
+        displayBox("Words",words);
+        displayBox("Scores",scores);
+        displayBox("Prices",prices);
+
+    }
+    public static <T> void displayBox(String label, SwapBox<T> box){
+        System.out.println( label +" before swap"+ box.describe());
+        box.swap();
+        System.out.println( label + " Words after swap"+ box.describe());
+    }
 }
--- src/main/java/com/pluralsight/generics/SwapBox.java
+++ src/main/java/com/pluralsight/generics/SwapBox.java
@@ -1,4 +1,36 @@
 package com.pluralsight.generics;
 
-public class SwapBox {
+public class SwapBox<T>{
+    private T leftItem;
+    private T rightItem;
+
+    public SwapBox(T leftItem, T rightItem) {
+        this.leftItem = leftItem;
+        this.rightItem = rightItem;
+    }
+    public T getLeftItem(){
+        return this.leftItem;
+    }
+
+    public T getRightItem() {
+        return this.rightItem;
+    }
+
+    public void setLeftItem(T leftItem) {
+        this.leftItem = leftItem;
+    }
+
+    public void setRightItem(T rightItem) {
+        this.rightItem = rightItem;
+    }
+
+    public void swap(){
+        T temp = this.leftItem;
+        this.leftItem = this.rightItem;
+        this.rightItem = temp;
+    }
+
+    public String describe(){
+        return String.format("Left: %s, Right: %s",this.leftItem.toString() ,this.rightItem.toString());
+    }
 }
